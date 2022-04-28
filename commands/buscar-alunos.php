5d43ac258197039b0e4a4d238f58283e002051f7
joaodematejr <joaodematejr@gmail.com>
2022-04-28 11:01:43 -0300

    Create Update Studant

--- commands/buscar-alunos.php
+++ commands/buscar-alunos.php
@@ -30,4 +30,19 @@ $alunoTeste7 = $alunoRepository->findOneBy([
     'nome' => 'Aluno TESTE 7'
 ]);
 
+/* public function findBy(
+    array $criteria,
+    ?array $orderBy = null,
+    ?int $limit = null,
+    ?int $offset = null
+) */
+
+
+/* $alunoRepository->findBy([], ['nome' => 'ASC'], 2, 3); */
+
+/* $criteria: Critério de busca. Array vazio significa sem critério, ou seja, sem filtro, buscando todos os registros;
+$orderBy: Critério de ordenação. Um array onde as chaves são os campos, e os valores são 'ASC' para ordem crescente e 'DESC' para decrescente;
+$limit: Numéro de resultados para trazer do banco;
+$offset: A partir de qual dado buscar do banco. Muito utilizado para realizar paginação de dados. */
+
 var_dump($alunoTeste7);
